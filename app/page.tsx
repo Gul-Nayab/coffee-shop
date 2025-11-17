bff9c4fd01d79a0cfd8de0c75847387841510da2
Gul Nayab <gngulnayab@gmail.com>
2025-11-17 13:58:17 -0800

    update landing splash.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,35 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 export default function Home() {
-  return <div> Hello</div>;
+  return (
+    <>
+      {/* navbar */}
+      <div>
+        <Image
+          src='/SJCoffeeLogo.png'
+          width={100}
+          height={100}
+          alt='Picture of the author'
+        />
+        <button>
+          <Link href={`/auth/create`} />
+          Sign Up
+        </button>
+        <button>
+          <Link href={`/auth/login`} />
+          Login In
+        </button>
+      </div>
+      <div>
+        <Image
+          src='/SJCoffeeLogo.png'
+          width={500}
+          height={500}
+          alt='Picture of the author'
+        />
+        <h1>WELCOME DOWNTOWN SAN JOSE COFFEE SHOP MANAGEMENT PLATFORM</h1>
+      </div>
+    </>
+  );
 }
