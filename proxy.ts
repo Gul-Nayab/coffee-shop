b4ab15cd6e19f034e5afaabd5bf8d32fc7f83b8a
Gul Nayab <gngulnayab@gmail.com>
2025-11-17 14:28:58 -0800

    fix proxy to make landing splash publicly available.

--- proxy.ts
+++ proxy.ts
@@ -6,8 +6,9 @@ export default auth((req) => {
   const { pathname } = req.nextUrl;
 
   const isPublic =
+    pathname === '/' ||
     pathname.startsWith('/auth') ||
-    pathname.startsWith('/api') ||
+    pathname.startsWith('/api') || //will remove later
     pathname.startsWith('/_next') ||
     pathname.startsWith('/favicon') ||
     pathname.startsWith('/images');
