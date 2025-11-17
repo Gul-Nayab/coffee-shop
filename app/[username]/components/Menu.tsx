6a30439c5b30cc1c961313dcce4218f39fa563d8
Gul Nayab <gngulnayab@gmail.com>
2025-11-17 14:21:43 -0800

    create menu component and route to get menu items with ingredients.

---
+++ app/[username]/components/Menu.tsx
@@ -0,0 +1,84 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { useSession } from 'next-auth/react';
+import { useUser } from '@/app/[username]/UserContext';
+import axios from 'axios';
+import { IconCookie, IconCup } from '@tabler/icons-react';
+
+interface MenuItem {
+  store_id: number;
+  item_name: string;
+  ingredients: Array<string>;
+  price: number;
+}
+
+function Menu({ store_id }: number) {
+  const router = useRouter();
+  //   const { data: session, status } = useSession();
+  const { user, userType, loading } = useUser();
+
+  const [menu, setMenu] = useState<MenuItem[]>([]);
+
+  //   useEffect(() => {
+  //     if (status === 'unauthenticated') {
+  //       router.push('/auth/login');
+  //     }
+  //   }, [status, router]);
+
+  useEffect(() => {
+    async function getMenuItems() {
+      try {
+        const response = await axios.get(
+          `/api/coffee-shop/stores/${store_id}/menu`,
+          {
+            timeout: 5000,
+          }
+        );
+        setMenu(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    getMenuItems();
+  }, [store_id]);
+
+  //   if (status === 'loading' || loading) {
+  //     return <div>Loading...</div>;
+  //   }
+
+  //   if (!session) {
+  //     return null;
+  //   }
+
+  function handleOrder(item_name: string) {
+    console.log(`${item_name} added to order`);
+  }
+
+  return (
+    <div>
+      {/*Menu Item container */}
+      {userType === 'customer' &&
+        menu.map((item) => (
+          <div key={item.item_name} onClick={() => handleOrder(item.item_name)}>
+            <div>
+              {item.ingredients.length === 0 ? (
+                <IconCookie size={20} />
+              ) : (
+                <IconCup size={20} />
+              )}
+            </div>
+            <h4>{item.item_name}</h4>
+            {item.ingredients.length !== 0 && (
+              <p>{item.ingredients.join(', ')}</p>
+            )}
+            <p>${item.price}</p>
+          </div>
+        ))}
+    </div>
+  );
+}
+
+export default Menu;
---
+++ app/api/coffee-shop/stores/[store_id]/menu/route.ts
@@ -0,0 +1,61 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { query } from '@/app/lib/db';
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: Promise<{ store_id: string }> }
+) {
+  try {
+    const { store_id } = await params;
+    if (!store_id) {
+      return NextResponse.json(
+        { error: 'store_id is required' },
+        { status: 400 }
+      );
+    }
+
+    const coffeeShopMenu = await query(
+      `SELECT m.store_id, m.item_name, i.ingredient_name, m.price 
+        FROM item m 
+        LEFT JOIN ingredient i ON i.item_name = m.item_name AND i.store_id = m.store_id
+        WHERE m.store_id = ?
+        ORDER BY m.item_name, i.amount_used DESC`,
+      [store_id]
+    );
+    console.log('coffeeShopMenu in api', coffeeShopMenu);
+
+    if (coffeeShopMenu.length === 0) {
+      return NextResponse.json(
+        { message: 'coffeeShopMenu not found' },
+        { status: 404 }
+      );
+    }
+
+    const grouped = coffeeShopMenu.reduce((acc: unknown, row: unknown) => {
+      const key = row.item_name;
+      if (!acc[key]) {
+        acc[key] = {
+          store_id: row.store_id,
+          item_name: row.item_name,
+          price: row.price,
+          ingredients: [],
+        };
+      }
+
+      if (row.ingredient_name) {
+        acc[key].ingredients.push(row.ingredient_name);
+      }
+      return acc;
+    }, {});
+    const result: unknown = Object.values(grouped);
+
+    return NextResponse.json(result);
+    // return NextResponse.json(coffeeShop);
+  } catch (error: unknown) {
+    console.error('Database error:', error);
+    return NextResponse.json(
+      { error: 'Failed to fetch coffeeShop menu' },
+      { status: 500 }
+    );
+  }
+}
