7afcb2a19e0e71ff3e0cf5b52afc58f7889cf715
Gul Nayab <gngulnayab@gmail.com>
2025-11-17 22:14:17 -0800

    create inventory page display.

---
+++ app/[username]/inventory/page.tsx
@@ -0,0 +1,150 @@
+'use client';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { useSession } from 'next-auth/react';
+import { useUser } from '../UserContext';
+import axios from 'axios';
+
+interface InventoryEntry {
+  store_id: number;
+  ingredient_name: string;
+  count: number;
+}
+
+function Inventory() {
+  const router = useRouter();
+  const { data: session, status } = useSession();
+  const { user, userType, loading } = useUser();
+
+  const [inventory, setInventory] = useState<InventoryEntry[]>([]);
+  const [increaseIngredientInventory, setIncreaseIngredientInventory] =
+    useState<string | null>('');
+  const [amountToAdd, setAmountToAdd] = useState<number>(0);
+
+  async function getInventory() {
+    try {
+      const response = await axios.get(
+        `/api/coffee-shop/stores/${user?.store_id}/inventory/`,
+        {
+          timeout: 5000,
+        }
+      );
+      setInventory(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    if (status === 'unauthenticated') router.push('/auth/login');
+    if (userType === 'customer' || userType === 'student')
+      router.push('/auth/login'); //add alert
+  }, [status, router, userType]);
+
+  useEffect(() => {
+    async function fetch() {
+      if (user?.store_id) {
+        getInventory();
+      }
+    }
+    fetch();
+  }, [user?.store_id]);
+
+  async function addInventory(
+    store_id: number,
+    ingredient_name: string,
+    amount_to_add: number
+  ) {
+    try {
+      const response = await axios.patch(
+        `/api/coffee-shop/stores/${user?.store_id}/inventory/`,
+        {
+          store_id,
+          ingredient_name,
+          amount_to_add,
+        }
+      );
+      alert(`${response.data.message}`);
+      setIncreaseIngredientInventory(null);
+      setAmountToAdd(0);
+      await getInventory();
+    } catch (error: unknown) {
+      console.error('Error adding inventory:', error);
+      alert(`Error:${error}`);
+    }
+  }
+
+  if (status === 'loading' || loading) return <div>Loading...</div>;
+  return (
+    <div>
+      <h1> Inventory</h1>
+      <table>
+        <thead>
+          <tr>
+            <th> Ingredient Name </th>
+            <th> Amount by Unit </th>
+          </tr>
+        </thead>
+        <tbody>
+          {inventory.map((entry, index) => (
+            <tr key={`${entry.store_id}-ingred-${index}`}>
+              {/* Display ingredient and amount */}
+              <td> {entry.ingredient_name}</td>
+              <td> {entry.count}</td>
+
+              {/* If user is manager, show inventory buttons.
+                  onclick of button, open input, on submit or cancel, 
+                  close input and show button again
+               */}
+              {userType === 'manager' && (
+                <td>
+                  {increaseIngredientInventory === entry.ingredient_name ? (
+                    <div style={{ display: 'flex', gap: '4px' }}>
+                      <input
+                        type='number'
+                        min='0'
+                        value={amountToAdd || 0}
+                        onChange={(e) =>
+                          setAmountToAdd(parseFloat(e.target.value))
+                        }
+                        style={{ width: '80px' }}
+                        placeholder='Amount'
+                      />
+                      <button
+                        onClick={() => {
+                          addInventory(
+                            entry.store_id,
+                            entry.ingredient_name,
+                            amountToAdd
+                          );
+                        }}
+                        disabled={amountToAdd === 0}
+                      >
+                        Add
+                      </button>
+                      <button
+                        onClick={() => setIncreaseIngredientInventory(null)}
+                      >
+                        Cancel
+                      </button>
+                    </div>
+                  ) : (
+                    //actual basic button for adding (opens input)
+                    <button
+                      onClick={() =>
+                        setIncreaseIngredientInventory(entry.ingredient_name)
+                      }
+                    >
+                      Add Inventory
+                    </button>
+                  )}
+                </td>
+              )}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+export default Inventory;
