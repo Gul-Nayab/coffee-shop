74272545ff521748e654809f774380c1051da8cf
Gul Nayab <gngulnayab@gmail.com>
2025-11-22 18:04:47 -0800

    add change password and delete account route endpoints.

--- app/api/coffee-shop/users/[username]/route.ts
+++ app/api/coffee-shop/users/[username]/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { query } from '@/app/lib/db';
+import { query, getConnection } from '@/app/lib/db';
 
 export async function GET(
   request: NextRequest,
@@ -24,6 +24,66 @@ export async function GET(
       return NextResponse.json({ message: 'User not found' }, { status: 404 });
     }
     return NextResponse.json(user[0]); // Return the found user
+  } catch (error: unknown) {
+    console.error('Database error:', error);
+    return NextResponse.json(
+      { error: 'Failed to fetch user ' },
+      { status: 500 }
+    );
+  }
+}
+
+export async function PATCH(
+  request: NextRequest,
+  { params }: { params: Promise<{ username: string }> }
+) {
+  const { username } = await params;
+  const body = await request.json();
+  const { newPassword } = body;
+
+  if (!username || !newPassword) {
+    return NextResponse.json(
+      { error: 'newPassword and username is required' },
+      { status: 400 }
+    );
+  }
+
+  const connection = await getConnection();
+  try {
+    // await connection.beginTransaction();
+    // await connection.commit();
+    return NextResponse.json({ message: 'Password changed!' });
+  } catch (error: unknown) {
+    await connection.rollback();
+    console.error('Transaction failed:', error);
+    return NextResponse.json(
+      { error: error.message || 'Failed to change password' },
+      { status: 500 }
+    );
+  } finally {
+    connection.release();
+  }
+}
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: Promise<{ username: string }> }
+) {
+  try {
+    const { username } = await params;
+    console.log('username: ', username);
+    if (!username) {
+      return NextResponse.json(
+        { error: 'Username is required' },
+        { status: 400 }
+      );
+    }
+
+    // const user = await query();
+    // if (user.length === 0) {
+    //   return NextResponse.json({ message: 'User not found' }, { status: 404 });
+    // }
+    return NextResponse.json({ message: 'User deleted successfully' }); // Return the found user
   } catch (error: unknown) {
     console.error('Database error:', error);
     return NextResponse.json(
