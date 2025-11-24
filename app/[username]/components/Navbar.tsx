a42af1ff75281f252b0150b07f6e85733676ab5f
Gul Nayab <gngulnayab@gmail.com>
2025-11-23 21:16:00 -0800

    add navbar styles.

--- app/[username]/components/Navbar.tsx
+++ app/[username]/components/Navbar.tsx
@@ -13,18 +13,19 @@ import {
   IconClock12,
   IconCoffee,
   IconLogout,
-  IconShoppingCart,
   IconShoppingCartFilled,
   IconTable,
   IconToolsKitchen2,
   IconUserFilled,
 } from '@tabler/icons-react';
+import './styles/Navbar.css';
 
 function NavBar() {
   const router = useRouter();
   const { data: session, status } = useSession();
   const { username } = useParams() as { username: string };
   const { userType, loading } = useUser();
+
   useEffect(() => {
     if (status === 'unauthenticated') router.push('/auth/login');
   }, [status, router]);
@@ -32,79 +33,67 @@ function NavBar() {
   if (status === 'loading' || loading) return <div>Loading...</div>;
 
   return (
-    <nav>
-      <ul style={{ display: 'flex', listStyle: 'none', gap: '2rem' }}>
+    <nav className='navbar'>
+      <div className='navbar-left'>
         <Link href={`/${username}/`}>
           <Image
             src='/images/SJCoffeeLogo.png'
-            width={50}
-            height={50}
+            width={65}
+            height={65}
             alt='logo'
+            className='navbar-logo'
           />
         </Link>
+      </div>
+
+      <div className='navbar-right'>
         {userType === 'customer' || userType === 'student' ? (
           <>
-            <li>
-              <Link href={`/${username}/stores`}>
-                <IconBuildingStore />
-              </Link>
-            </li>
-            <li>
-              <Link href={`/${username}/orders`}>
-                <IconToolsKitchen2 />
-              </Link>
-            </li>
-            <li>
-              <Link href={`/${username}/cart`}>
-                <IconShoppingCartFilled color='#1f130f' />
-              </Link>
-            </li>
+            <Link href={`/${username}/cart`}>
+              <IconShoppingCartFilled className='nav-icon' />
+            </Link>
+            <Link href={`/${username}/stores`}>
+              <IconBuildingStore className='nav-icon' />
+            </Link>
+            <Link href={`/${username}/account`}>
+              <IconUserFilled className='nav-icon' />
+            </Link>
           </>
         ) : (
           <>
-            <li>
-              <Link href={`/${username}/`}>
-                <IconCoffee />
-              </Link>
-            </li>
-            <li>
-              <Link href={`/${username}/shifts`}>
-                <IconClock12 />
-              </Link>
-            </li>
-            <li>
-              <Link href={`/${username}/inventory`}>
-                <IconTable />
-              </Link>
-            </li>
+            <Link href={`/${username}/`}>
+              <IconCoffee className='nav-icon' />
+            </Link>
+            <Link href={`/${username}/shifts`}>
+              <IconClock12 className='nav-icon' />
+            </Link>
+            <Link href={`/${username}/inventory`}>
+              <IconTable className='nav-icon' />
+            </Link>
             {userType === 'manager' ? (
-              <li>
-                <Link href={`/${username}/finances`}>
-                  <IconBusinessplan />
-                </Link>
-              </li>
+              <Link href={`/${username}/finances`}>
+                <IconBusinessplan className='nav-icon' />
+              </Link>
             ) : (
-              <li>
-                <Link href={`/${username}/orders`}>
-                  <IconCategory2 />
-                </Link>
-              </li>
+              <Link href={`/${username}/orders`}>
+                <IconCategory2 className='nav-icon' />
+              </Link>
             )}
+            <Link href={`/${username}/account`}>
+              <IconUserFilled className='nav-icon' />
+            </Link>
           </>
         )}
 
-        <li>
-          <button onClick={() => signOut({ callbackUrl: '/auth/login' })}>
-            <IconLogout />
-          </button>
-        </li>
-        <li>
-          <Link href={`/${username}/account`}>
-            <IconUserFilled />
-          </Link>
-        </li>
-      </ul>
+        <button
+          onClick={() => signOut({ callbackUrl: '/auth/login' })}
+          className='logout-btn'
+        >
+          <IconLogout className='nav-icon' />
+        </button>
+      </div>
     </nav>
   );
 }
+
 export default NavBar;
